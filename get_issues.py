42a6a9e082aaa6d7800cf978d9a2839a9943f6ce
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-04-19 15:02:28 +0530

    Added comments

--- get_issues.py
+++ get_issues.py
@@ -4,6 +4,7 @@ import requests
 response = requests.get('https://api.github.com/users/coding-blocks/repos').json()
 
 for i in response:
-    if int(i['open_issues_count']) > 0:
-        r = requests.get('{}/{}'.format(i['url'],'issues'))
-        print(r.text)
+    if int(i['open_issues_count']) > 0: # Check if repo has open issues
+        r = requests.get('{}/{}'.format(i['url'],'issues')).json() # Get repos issues
+        for j in r:
+            print(j['url'])
