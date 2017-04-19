3af155afaadb2c80175b30ca271012a1d3e7d74b
Anshul <malikanshul29@gmail.com>
2017-04-19 12:42:29 +0000

    Add github authentication

--- get_issues.py
+++ get_issues.py
@@ -1,9 +1,32 @@
-from pprint import pprint
 import requests
+import sys
+import getpass
+from requests.auth import HTTPBasicAuth
 
-response = requests.get('https://api.github.com/users/coding-blocks/repos').json()
 
-for i in response:
-    if int(i['open_issues_count']) > 0:
-        r = requests.get('{}/{}'.format(i['url'],'issues'))
-        print(r.text)
+def getIssuesWithLabel(username, password, label='BOSS'):
+    repos = requests.get('https://api.github.com/users/coding-blocks/repos', auth=HTTPBasicAuth(username, password)).json()
+    bossIssues = []
+    
+    for repo in repos:
+        if int(repo['open_issues_count']) > 0:
+            issues = requests.get('{}/{}'.format(repo['url'], 'issues')).json()
+    
+            for issue in issues:
+                labels = [label['name'] for label in issue['labels']]
+                if 'BOSS' in labels:
+                    issue['repo'] = repo
+                    
+                    bossIssues.append(issue)
+                    
+    return bossIssues
+
+# Use github authentication since we get only 60 API requests
+# with unauthenticated requests
+
+username = raw_input("Enter github username: ")
+password = getpass.getpass("Enter password: ")
+
+issues = getIssuesWithLabel(username, password)
+for issue in issues:
+    print(issue['repo']['name'] + " : " + issue['title'])
